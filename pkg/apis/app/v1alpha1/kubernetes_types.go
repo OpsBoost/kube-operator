94211ef7b364379b2c5e98a35866a2edbd0a2359
steigr <me@stei.gr>
2019-12-27 18:59:05 +0100

    Added version for KinK

--- pkg/apis/app/v1alpha1/kubernetes_types.go
+++ pkg/apis/app/v1alpha1/kubernetes_types.go
@@ -12,6 +12,7 @@ type KubernetesSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	Version string `json:"version,omitempty"`
 }
 
 // KubernetesStatus defines the observed state of Kubernetes
